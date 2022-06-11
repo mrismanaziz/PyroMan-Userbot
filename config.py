0ae70f8331815e472443d40b7cb8f8500b444785
mrismanaziz <mrismanaziz@gmail.com>
2022-06-12 01:31:34 +0700

    config: update git_token

--- config.py
+++ config.py
@@ -7,6 +7,7 @@
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
 
+from base64 import b64decode
 from distutils.util import strtobool
 from os import getenv
 
@@ -26,16 +27,16 @@ BRANCH = "main"
 CHANNEL = getenv("CHANNEL", "Lunatic0de")
 CMD_HANDLER = getenv("CMD_HANDLER", ".")
 DB_URL = getenv("DATABASE_URL", "")
-GIT_TOKEN = getenv("GIT_TOKEN", "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")
+GIT_TOKEN = getenv("GIT_TOKEN", b64decode("Z2hwXzg1Z1I1RlRRanRRTG9TU2FaRGVrcnI4c1pBSDB6cjRRMlN5UA==").decode("utf-8"))
 GROUP = getenv("GROUP", "SharingUserbot")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
 PM_AUTO_BAN = strtobool(getenv("PM_AUTO_BAN", "True"))
-REPO_URL = "https://github.com/mrismanaziz/PyroMan"
+REPO_URL = getenv("REPO_URL", "https://github.com/projectm4n/PyroMan")
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
 STRING_SESSION5 = getenv("STRING_SESSION5", "")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
