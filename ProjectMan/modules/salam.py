ad027bfd54294d4fcabf85bba5a2ae9e27a34858
mrismanaziz <mrismanaziz@gmail.com>
2022-06-20 22:23:30 +0700

    refactored send salam with asyncio.gather()

--- ProjectMan/modules/salam.py
+++ ProjectMan/modules/salam.py
@@ -7,7 +7,7 @@
 #
 # t.me/SharingUserbot & t.me/Lunatic0de
 
-from asyncio import sleep
+import asyncio
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -21,67 +21,77 @@ from .help import add_command_help
 
 @Client.on_message(filters.command("p", cmd) & filters.me)
 async def salamone(client: Client, message: Message):
-    await client.send_message(
-        message.chat.id,
-        "Assalamualaikum",
-        reply_to_message_id=ReplyCheck(message),
+    await asyncio.gather(
+        message.delete(),
+        client.send_message(
+            message.chat.id,
+            "Assalamualaikum",
+            reply_to_message_id=ReplyCheck(message),
+        ),
     )
-    await message.delete()
 
 
 @Client.on_message(filters.command("pe", cmd) & filters.me)
 async def salamdua(client: Client, message: Message):
-    await client.send_message(
-        message.chat.id,
-        "Assalamualaikum Warahmatullahi Wabarakatuh",
-        reply_to_message_id=ReplyCheck(message),
+    await asyncio.gather(
+        message.delete(),
+        client.send_message(
+            message.chat.id,
+            "Assalamualaikum Warahmatullahi Wabarakatuh",
+            reply_to_message_id=ReplyCheck(message),
+        ),
     )
-    await message.delete()
 
 
 @Client.on_message(filters.command("l", cmd) & filters.me)
 async def jwbsalam(client: Client, message: Message):
-    await client.send_message(
-        message.chat.id, "Wa'alaikumsalam", reply_to_message_id=ReplyCheck(message)
+    await asyncio.gather(
+        message.delete(),
+        client.send_message(
+            message.chat.id,
+            "Wa'alaikumsalam",
+            reply_to_message_id=ReplyCheck(message),
+        ),
     )
-    await message.delete()
 
 
 @Client.on_message(filters.command("el", cmd) & filters.me)
 async def jwbsalamlngkp(client: Client, message: Message):
-    await client.send_message(
-        message.chat.id,
-        "Wa'alaikumsalam Warahmatullahi Wabarakatuh",
-        reply_to_message_id=ReplyCheck(message),
+    await asyncio.gather(
+        message.delete(),
+        client.send_message(
+            message.chat.id,
+            "Wa'alaikumsalam Warahmatullahi Wabarakatuh",
+            reply_to_message_id=ReplyCheck(message),
+        ),
     )
-    await message.delete()
 
 
 @Client.on_message(filters.command("a", cmd) & filters.me)
 async def salken(client: Client, message: Message):
     xx = await edit_or_reply(message, f"**Haii Salken Saya {client.me.first_name}**")
-    await sleep(2)
+    await asyncio.sleep(2)
     await xx.edit("Assalamualaikum")
 
 
 @Client.on_message(filters.command("ass", cmd) & filters.me)
 async def salamarab(client: Client, message: Message):
     xx = await edit_or_reply(message, "Salam Dulu Biar Sopan")
-    await sleep(2)
+    await asyncio.sleep(2)
     await xx.edit("السَّلاَمُ عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ")
 
 
 @Client.on_message(filters.command("j", cmd) & filters.me)
 async def jakasem(client: Client, message: Message):
     xx = await edit_or_reply(message, "**JAKA SEMBUNG BAWA GOLOK**")
-    await sleep(3)
+    await asyncio.sleep(3)
     await xx.edit("**NIMBRUNG GOBLOKK!!!🔥**")
 
 
 @Client.on_message(filters.command("k", cmd) & filters.me)
 async def ngegas(client: Client, message: Message):
     xx = await edit_or_reply(message, f"**Hallo KIMAAKK SAYA {client.me.first_name}**")
-    await sleep(2)
+    await asyncio.sleep(2)
     await xx.edit("**LU SEMUA NGENTOT 🔥**")
 
 
