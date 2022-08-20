5b96363a5655efff6e316b2ef09de0f1a01c8b54
mrismanaziz <mrismanaziz@gmail.com>
2022-08-20 15:25:35 +0700

    fix mistake

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ API_ID = int(getenv("API_ID", ""))
 BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
 if not BLACKLIST_CHAT:
     BLACKLIST_CHAT = [-1001473548283]
-BLACKLIST_GCAST = {int(x) for x in os.environ.get("BLACKLIST_GCAST", "").split()}
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 BOT_VER = "0.2.0@main"
 BRANCH = getenv("BRANCH", "v2")
