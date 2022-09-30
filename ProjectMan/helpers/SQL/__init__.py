7a42b4d5d4b30de381e422e012a1c6d3d33c4fe3
mrismanaziz <mrismanaziz@gmail.com>
2022-09-30 13:17:02 +0700

    unused

--- ProjectMan/helpers/SQL/__init__.py
+++ ProjectMan/helpers/SQL/__init__.py
@@ -1,6 +1,5 @@
 import os
 
-import motor.motor_asyncio
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
